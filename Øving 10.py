2236a104f4b0c4d71fffda44a7de4ebde8fefc7d
Umbrako <116559190+Umbrako@users.noreply.github.com>
2022-11-09 11:17:28 +0000

    Update Øving 10.py

--- Øving 10.py
+++ Øving 10.py
@@ -2,184 +2,268 @@
 #                     Oppgave d
 # -----------------------------------------------------------
 
-import json 
+import json
 from datetime import datetime
+from typing import List, Optional
+
 
 class Avtale:
-  def __init__(self, tittel, sted, start, varighet):
-    self.tittel = tittel
-    self.sted = sted
-    self.start = start
-    self.varighet = varighet
-    
-    def __str__(self, tittel, sted, start, varighet):
-          return f"tittel={self.tittel}, sted ={self.sted}, start={self.start}, varighet={varighet}"
-    
-  
-    
-now = datetime.now()
-dag = now.strftime("%H:%M:%S:")
-print(datetime.fromisoformat("2000-09-20 12:00:00"))
+    def __init__(self, tittel: str, sted: str, start: datetime, varighet: int):
+        self.tittel = tittel
+        self.sted = sted
+        self.start = start
+        self.varighet = varighet
 
-# -----------------------------------------------------------
-#                     Oppgave e
-# -----------------------------------------------------------
-
-
-
-
-avtale = Avtale("Kvernevik", "Hafrsfjord", "start", "varighet")
-
-
-
-print(avtale.tittel)   
-print(avtale.sted) 
-
+    # Oppgave e
+    def __str__(self):
+        return f"tittel={self.tittel}, sted={self.sted}, start={self.start}, varighet={self.varighet}"
 
 
 # -----------------------------------------------------------
 #                     Oppgave f
 # -----------------------------------------------------------
-def nyAvtale(self, tittel, sted, start, varighet):
-    self.tittel = input("Tittel:")
-    self.sted = input("Sted:")
-    self.start = datetime.fromisoformat(input("Start (ÅÅÅÅ-MM-DD HH:MM:SS):"))
-    self.varighet =input("Varighet: ")
-    return f"tittel={self.tittel}, sted ={self.sted}, start={self.start}, varighet={varighet}"
- 
-    
-    
 
-print()
-print("Avtale 2")
-print(nyAvtale.tittel) 
-print(nyAvtale.sted)
-print(nyAvtale.start)
-print(nyAvtale.varighet,"timer")
-print()
+# Kommentaren kan utvides
+def lag_ny_avtale() -> Optional[Avtale]:
+    """Registrer en ny avtale"""
 
-        
+    tittel = input("Skriv inn avtale tittel # ")
+    sted = input("Skriv inn avtale sted # ")
+    start = input("Skriv inn avtale start 'ÅÅÅÅ-MM-DD HH:MM:SS' # ")
+    varighet = input("Skriv inn avtale varighet (minutter) # ")
+
+    # Sjekk om vi kan konvertere start til et datetime objekt
+    try:
+        start_tid = datetime.fromisoformat(start)
+    except ValueError:
+        print("Dato-formatet du oppgav ble ikke gjenkjent, bruk 'ÅÅÅÅ-MM-DD HH:MM:SS'")
+        return None
+
+    # Sjekk om varighet er et tall
+    try:
+        varighet_int = int(varighet)
+    except ValueError:
+        print("Du må skrive inn et gyldig tall for varighet!")
+        return None
+
+    return Avtale(tittel, sted, start_tid, varighet_int)
+
+
 # -----------------------------------------------------------
 #                     Oppgave g
 # -----------------------------------------------------------
+def skriv_ut_avtaler(avtaler: List[Avtale], overskrift=None) -> None:
+    """Skriv ut en liste med avtaler til skjermen"""
 
-    
-def Banken(self, tittel, sted, start, varighet):
-        
-        return f"tittel={self.tittel}, sted ={self.sted}, start={self.start}, varighet ={self.varighet}"
- 
-deal = Avtale("Bankkort","DNB Arena",datetime.fromisoformat("2022-01-11 12:00:00"), "2 timer")
-print()
-print("Banken")
-print(deal.tittel)
-print(deal.sted)
-print(deal.start)
-print(deal.varighet)
+    # Pga. overskrift er frivillig må vi først se om overskrift ble oppgitt
+    if overskrift:
+        print(overskrift)
+
+    # Loop gjennom listen og skriv ut indeks + tittel til avtale
+    for i in range(len(avtaler)):
+        print(f"#{i} {avtaler[i].tittel}")
 
 
 # -----------------------------------------------------------
 #                     Oppgave h
 # -----------------------------------------------------------
 
-avtaler = []
-avtaler.append(Avtale)
-avtaler.append(deal)
-    
-json_object = json.dumps({'avtaler': avtaler}, indent=4)
+# Hentet fra https://stackoverflow.com/a/60035604
+# og https://stackoverflow.com/a/35780962
+# Vi trenger denne for å konvertere datetime object til string
+def datetime_option(obj):
+    if isinstance(obj, datetime):
+        return obj.strftime("%Y-%m-%d %H:%M:%S")
+    else:
+        return obj.__dict__
 
-with open("test.json", "w") as outfile:
-    outfile.open(json_object)
-    
-    
-        
+
+def skriv_avtaler_til_fil(avtaler: List[Avtale]) -> None:
+    """Skriv liste med avtaler til json fil"""
+    json_object = json.dumps(avtaler, default=datetime_option,
+                             indent=4, separators=(',', ': '))
+
+    with open("avtale_lister.json", "w") as json_fil:
+        json_fil.write(json_object)
+
+
 # -----------------------------------------------------------
 #                     Oppgave i
 # -----------------------------------------------------------
-with open("test.json", "r") as outfile:
-    print(outfile.read())
-    
+
+
+def les_avtaler_fra_fil() -> List[Avtale]:
+    """Les inn alle avtaler fra fil og returner i liste"""
+    avtaler = []
+    with open("avtale_lister.json", "r") as json_fil:
+        json_data = json.load(json_fil)
+
+        # Det er trygt å anta at typene til
+        # variablene er korrekt pga. vi skriver
+        # bare gyldig "avtaler" til filen
+        for avtale_json in json_data:
+            tittel = avtale_json["tittel"]
+            sted = avtale_json["sted"]
+            start = datetime.fromisoformat(avtale_json["start"])
+            varighet = int(avtale_json["varighet"])
+
+            ny_avtale = Avtale(tittel, sted, start, varighet)
+            avtaler.append(ny_avtale)
+
+        return avtaler
+
+
 # -----------------------------------------------------------
 #                     Oppgave j
 # -----------------------------------------------------------
-def alle_avtaler_paa_dato(avtaler, dato):
+def alle_avtaler_paa_dato(avtaler: List[Avtale], dato: datetime):
+    """Returner alle avtale på bestemt dato"""
     avtaler_paa_dato = []
     for avtale in avtaler:
-        avtale_dato = datetime.fromisoformat(avtale.start)
-        if avtale_dato >= dato:
-            avtaler.append(avtale)
-        return avtaler_paa_dato
+        if avtale.start == dato:
+            avtaler_paa_dato.append(avtale)
+    return avtaler_paa_dato
+
 
 # -----------------------------------------------------------
 #                     Oppgave k
 # -----------------------------------------------------------
 
-def get_tittel(self):
-    return self.tittel
+def sok_etter_avtale(avtaler: List[Avtale], soke_ord_liste: List[str]):
+    """Søk etter en avtale i en liste"""
+
+    sok_resultat = []
+    for avtale in avtaler:
+
+        # Gjør søket ufølsomt mot små/store bokstaver
+        avtale_tittel = avtale.tittel.lower()
+
+        # Lag en kopi av soke ordene
+        soke_ord_liste_kopi = soke_ord_liste.copy()
+        for soke_ord in soke_ord_liste:
+            if soke_ord in avtale_tittel:
+                soke_ord_liste_kopi.remove(soke_ord)
+
+            # Dersom soke_ord_liste_kopi er tom er søket fullført
+            if not soke_ord_liste_kopi:
+                sok_resultat.append(avtale)
+                continue
+
+    return sok_resultat
 
 
-
-avtale1 = Avtale("Avtale 1", "", "", "")
-avtale2 = Avtale("Avtale 2", "", "", "")
-avtale3 = Avtale("absssss", "", "", "")
-
-
-liste = [avtale1, avtale2, avtale3]
-
-
-def funksjon(liste, streng):
-    returnList = []
-    for Avtale in liste:
-        tittelValue = avtale.get_tittel()
-        if (tittelValue.find(streng) != -1):
-            returnList.append(avtale)
-    
-    return returnList
-
-
-
-print(funksjon(liste, "Avtale"))
-
-
-avtaler = funksjon(liste, "Avtale")
-for avtale in avtaler:
-    print(avtale.get_tittel())
- 
- 
 # -----------------------------------------------------------
-#                     Oppgave l
+#                     Oppgave l, m, n, o
 # -----------------------------------------------------------
 
-mylist = []
-def meny():
+
+def rediger_avtale(avtale: Avtale):
+    """Rediger en avtale"""
+
+    valg = input("""[1] Endre tittel
+[2] Endre sted
+[3] Endre start
+[4] Endre varighet
+Velg en operasjon # """)
+
+    if valg == "1":
+        ny_tittel = input(f"Skriv inn en ny tittel for avtalen '{avtale.tittel}' # ")
+        avtale.tittel = ny_tittel
+
+    elif valg == "2":
+        ny_sted = input(f"Skriv inn et nytt sted for avtalen '{avtale.tittel}' # ")
+        avtale.sted = ny_sted
+
+    elif valg == "3":
+        start = input(f"Skriv inn en ny start-tid for avtalen '{avtale.tittel}' 'ÅÅÅÅ-MM-DD HH:MM:SS' # ")
+
+        # Gjør om str til datetime
+        try:
+            ny_start = datetime.fromisoformat(start)
+            avtale.start = ny_start
+        except ValueError:
+            print("Dato-formatet du oppgav ble ikke gjenkjent, bruk 'ÅÅÅÅ-MM-DD HH:MM:SS'")
+
+    elif valg == "4":
+        vargihet = input(f"Skriv inn en ny varighet avtalen '{avtale.tittel}' # ")
+
+        try:
+            ny_varighet = int(vargihet)
+            avtale.varighet = ny_varighet
+        except ValueError:
+            print("Du oppga ikke en int!")
+
+
+def kjor_meny_system():
+
+    mine_avtaler = []
+    endret = False
 
     while True:
-        operation = input('''
-        Select operation:
-        [1] Lese inn avtaler fra fil
-        [2] Skrive avtalene til fil
-        [3] Skrive inn ny avtale
-        [4] Skrive ut alle avtalene
-        [5] Avslutt
+        valg = input("""[1] Lese inn avtaler fra fil
+[2] Skrive avtalene til fil
+[3] Lag ny avtale
+[4] Skrive ut alle avtalene
+[5] Rediger en avtale
+[6] Slett en avtale
+[7] Avslutt
+Velg en operasjon # """)
 
-        ''')
-        if operation == 1
-            print("")
+        if valg == "1":
+            # Spør om bekfreftelse før vi leser inn dersom mine_avtaler ikke er tom
+            if mine_avtaler:
+                bekreftelse = input("Vil du overskrive avtaler (Ja/Nei) # ").lower()
+                if bekreftelse == "ja":
+                    mine_avtaler = les_avtaler_fra_fil()
+            else:
+                mine_avtaler = les_avtaler_fra_fil()
 
-        elif operation == 2
-            print()
+        elif valg == "2":
+            skriv_avtaler_til_fil(mine_avtaler)
+            endret = False
 
-        elif operation == 3
+        elif valg == "3":
+            min_nye_avtale = lag_ny_avtale()
+            if isinstance(min_nye_avtale, Avtale):
+                mine_avtaler.append(min_nye_avtale)
+                endret = True
 
-        elif operation == 4
+        elif valg == "4":
+            overskrift = input("Skriv inn overskrift # ")
+            skriv_ut_avtaler(mine_avtaler, overskrift=overskrift)
 
-        elif operation == 5
+        elif valg == "5":
+            skriv_ut_avtaler(mine_avtaler)
+
+            valgt_indeks = input("Velg indeksen til avtalen du vil redigere # ")
+            try:
+                valgt_indeks_int = int(valgt_indeks)
+
+                valgt_avtale = mine_avtaler[valgt_indeks_int]
+                rediger_avtale(valgt_avtale)
+                endret = True
+            except ValueError:
+                print("Du oppga ikke en int!")
+
+        elif valg == "6":
+            skriv_ut_avtaler(mine_avtaler)
+
+            valgt_indeks = input("Velg indeksen til avtalen du vil slette # ")
+            try:
+                valgt_indeks_int = int(valgt_indeks)
+
+                del mine_avtaler[valgt_indeks_int]
+                endret = True
+            except ValueError:
+                print("Du oppga ikke en int!")
+
+        elif valg == "7":
+            if endret:
+                lagre_valg = input("Vil du lagre ? (Ja/Nei) ").lower()
+                if lagre_valg == "ja":
+                    skriv_avtaler_til_fil(mine_avtaler)
             break
-# -----------------------------------------------------------
-#                     Oppgave m
-# -----------------------------------------------------------
-# 
-# 
-# 
-# -----------------------------------------------------------
-#                     Oppgave n
-# -----------------------------------------------------------
+
+
+if __name__ == '__main__':
+    kjor_meny_system()
